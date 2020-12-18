46dea38675ee33e078fab3b0b313af2b529d4702
Mikael Roos <mikael.t.h.roos@gmail.com>
2020-12-18 15:16:09 +0100

    Included mocha and phpunit in make all

--- src/0001-Convert-between-Celsius-and-Fahrenheit/.tomato/test/phpunit/config.php
+++ src/0001-Convert-between-Celsius-and-Fahrenheit/.tomato/test/phpunit/config.php
@@ -2,6 +2,6 @@
 /**
  * Configuration file for Anax webroot.
  */
-define("SOURCE", realpath(__DIR__ . "/../../src"));
-define("OUTPUT", realpath(__DIR__ . "/output"));
+define("SOURCE", realpath(__DIR__ . "/../../../src"));
+define("OUTPUT", realpath(__DIR__ . "/../../output"));
 #require ANAX_INSTALL_PATH . "/vendor/autoload.php";
